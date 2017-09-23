8542a9366a730c9a44110885d440e44f4c233342
Vincent <vincentnewkirk@gmail.com>
2017-09-23 09:36:48 +0900

    set up express with webpack

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,26 +65,28 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_lodash__);
 
+
+var _lodash = __webpack_require__(1);
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function component() {
   var element = document.createElement('div');
 
-// Lodash, currently included via a script, is required for this line to work
-// Lodash, now imported by this script
-  element.innerHTML = __WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['Hello', 'webpack'], ' ');
+  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now imported by this script
+  element.innerHTML = _lodash2.default.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
 
 document.body.appendChild(component());
-
 
 /***/ }),
 /* 1 */
